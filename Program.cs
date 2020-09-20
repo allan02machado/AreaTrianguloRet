54e9181e9be073fb57fbff20275054487c3e2020
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-09-20 20:03:35 -0300

    Este commit altera a página inicial do repositório

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double primeiroNumero;
+            double segundoNumero;
+            const double terceiroNumero = 2;
+
+            Console.WriteLine("Bem-vindo ao calcula triângulo retângulo");
+            Console.WriteLine("Pressione uma tecla para continuar...");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Digite os valores a serem calculados: ");
+
+            Console.Write("1o. numero: ");
+            primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("2o. numero: ");
+            segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = (primeiroNumero * segundoNumero) / terceiroNumero;
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"O resultado é {resultado:N1}");
+            Console.ResetColor();
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # AreaTrianguloRet
 Resposta exercício "AreaTrianguloRet"
+
+**Enunciado**
+
+Calcule a área de um triângulo retângulo, dados base (b) e altura (h). A área é dada pela metade do produto da base pela altura.
+
+**Como executar:**
+
+Abra o programa e digite:
+
+dotnet run
